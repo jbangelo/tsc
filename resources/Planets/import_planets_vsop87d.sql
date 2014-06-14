9059a136e285be4081da7e66f99ba06e7547d781
jbangelo <b.j.angelo@gmail.com>
2014-06-14 15:46:45 -0700

    Added all current .csv files and a sql script to import all of them into one sqlite file

--- resources/Planets/import_planets_vsop87d.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE planets (tid INT, rank INT, ml1 INT, ml2 INT, ml3 INT, ml4 INT, ml5 INT, ml6 INT, ml7 INT, ml8 INT, ml9 INT, ml10 INT, ml11 INT, ml12 INT, S DOUBLE, K DOUBLE, A DOUBLE, B DOUBLE, C DOUBLE);
-.mode csv
-.import planets_vsop87d.csv planets
