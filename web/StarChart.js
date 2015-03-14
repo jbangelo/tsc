832f9014db036bab5a7659d5582f5d9d533fe61c
Joseph Angelo <b.j.angelo@gmail.com>
2015-03-14 14:21:23 -0700

    Removed old python/web code and moved the library project into a lib directory

--- web/StarChart.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	$("#datepicker").datepicker({autoSize: true});
-	$("#timepicker").timepicker();
-
-	var timezone = jstz.determine();
-	$("#tzField").val(timezone.name());
-}
-
-
